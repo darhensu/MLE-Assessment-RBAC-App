b14730641443d5446c60f5e1ce447911087435d1
Suhendar Aditya <mochammad.120450058@student.itera.ac.id>
2025-01-17 16:34:55 +0700

    added data to rbac.db with add_data.py

--- add_data.py
+++ add_data.py
@@ -9,4 +9,19 @@ cursor.execute("insert into role(role_name) values('Editor')")
 cursor.execute("insert into role(role_name) values('Viewer')")
 
 # Menambah data untuk permission
-cursor.execute
+cursor.execute("insert into permission(permission_name) values('edit_document')")
+cursor.execute("insert into permission(permission_name) values('view_document')")
+
+# Menambah user dengan role
+cursor.execute("insert into user(username, email, role_id) values('suhendar','suhendar@example.com', 1)")
+cursor.execute("insert into user(username, email, role_id) values('aditya','aditya@example.com', 2)")
+
+# Menambah role permission
+cursor.execute("insert into rolepermission(role_id, permission_id) values(1, 1)") # Editor -> edit_document
+cursor.execute("insert into rolepermission(role_id, permission_id) values(2, 2)") # Viewer -> view_document
+
+# Menyimpan perubahan dan menutup koneksi
+conn.commit()
+conn.close()
+
+print("Data added successfully!")
